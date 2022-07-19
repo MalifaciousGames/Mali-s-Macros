5b427f06d64b92b227d30977e310333c2c3df18b
Maliface <105600295+MalifaciousGames@users.noreply.github.com>
2022-07-19 19:00:56 +0200

    Add files via upload

--- input.js
+++ input.js
@@ -3,51 +3,61 @@ Macro.add('input', {
 	
 	handler() {
 			
-		let varName = this.args[1].trim(),type = this.args[0], value;
+		let varName = this.args[1].trim(),type = this.args[0], value, varType;
 			
 		if (this.args.length < 2) {
 			return this.error('missing input type or associated variable');
 		}
 
-// Ensure that the variable name argument is a string.
+		// Ensure that the variable name argument is a string.
 
 		if (typeof this.args[1] !== 'string') {
-			return this.error(`variable name "${this.args[0]}" is not a string`);
+			return this.error(`variable name "${this.args[1]}" is not a string`);
 		}
 
-// Try to ensure that we receive the variable's name (incl. sigil), not its value.
+		// Setting varType
 			
-		if (varName[0] !== '$' && varName[0] !== '_') {
-			return this.error(`variable name "${this.args[0]}" is missing its sigil ($ or _)`);
+		if (varName[0] === '$' || varName[0] === '_') {
+			varType = 'state';
+		} else if (varName.split('.')[0] === 'setup') {
+			varType = 'setup';
+		} else if (varName.split('.')[0] === 'settings') {
+			varType = 'settings';
+		} else {
+			return this.error(`unrecognized variable type: "${this.args[1]}"`);
 		}
-
-// Creates input element, applies attributes
-
+			
 		const el  = jQuery(document.createElement('input'));
-
+		// Applies attributes to element
 
 		if (this.args.length % 2 === 0) {
 			for (let i = 2; i < this.args.length;i+=2) {
 				el.attr( this.args[i] , this.args[i+1] );
 			}
-			el.attr("type", type);
+		el.attr("type", type);
 		} else { 
-			return this.error('Number of arguments must be an even number: type $var (attribute value).');
+			return this.error('number of arguments must be even: type $var (attribute value).');
 		}
 
-// Listeners for change/enter
+		// Listeners for change/enter
 			
 		jQuery(el)
 			.on('change.macros', this.createShadowWrapper(function () {
 				value = isNaN(Number(this.value))||type === 'text'? this.value : Number(this.value);
 				State.setVar(varName, value);
+				if (varType === 'settings') {
+					Setting.save();
+				}
 			}))
-			
+				
 			.on('keypress.macros', this.createShadowWrapper(function (ev) {
 				if (ev.which === 13) { // Enter keycode
 					ev.preventDefault();
 					value = isNaN(Number(this.value))||type === 'text'? this.value : Number(this.value);
 					State.setVar(varName, value);
+					if (varType === 'settings') {
+						Setting.save();
+					}
 				}
 			}))
 		.appendTo(this.output);
