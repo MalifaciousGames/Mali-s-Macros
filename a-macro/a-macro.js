c6c821e99ae123230f12d830f9eb6bebcd273d7e
Maliface <105600295+MalifaciousGames@users.noreply.github.com>
2022-11-03 11:26:47 +0100

    Update a-macro.js

--- a-macro/a-macro.js
+++ a-macro/a-macro.js
@@ -1,99 +1,100 @@
-// The <<a>> macros, customizable interactive elements
-//Special properties: goto , rep/replace , prep/prepend , app/append
+/* Maliface's 'a' macro for Sugarcube
 
+Basic syntax: <<a 'Link text' [goto 'PassageName']>>...link contents...<</a>>
+
+Attributes syntax:
+- pairs : <<a 'Text' attribute value...>>>>...link contents...<</a>>
+- object : <<a 'Text' `{attribute : value}`>>...link contents...<</a>>
+
+Output options:
+<<a 'Text'>>
+	...silent code...
+<<rep 'selector'>> ...content to replace 'selector' with...
+<<prep 'selector'>> ...content to prepend to 'selector'...
+<<app 'selector'>> ...content to append to 'selector'...
+<</a>>*/
 
 Macro.add(['a','adel','but','butdel'], {
 	isAsync : true,
-	tags    : null,
+	tags    : ['rep','prep','app'],
 
 	handler() {
 		
-		let passage, toApp = [], toRep = [], toPrep = []; 
-		const payload = this.payload[0].contents.trim();
+		const type = (this.name[0] === 'b') ? 'button' : 'link', attributes = this.args.slice(1), payload = this.payload[0].contents.trim();
+		const Rep = this.payload.find(pay => pay.name === 'rep'),
+					Prep = this.payload.find(pay => pay.name === 'prep'),
+					App = this.payload.find(pay => pay.name === 'app');
 		
-		// Link is empty or number of args is odd
+		//Broad errors
 
 		if (this.args.length === 0) {
-			return this.error('No link text specified');
-		} else if (this.args.length % 2 != 1) {
-			return this.error('Number of arguments must be odd: "link text" + attribute/value pair');
+			return this.error(`No ${type} text specified.`);
 		}
-				
+		const error = [];
+		if (Rep && !Rep.args[0]){
+			error.push(`<<rep>>`);
+		}
+		if (Prep && !Prep.args[0]){
+			error.push(`<<prep>>`);
+		}
+		if (App && !App.args[0]){
+			error.push(`<<app>>`);
+		}
+		if (error.length){
+			return this.error(`Invalid selector supplied to ${error.join(', ')}.`);
+		}
+		
 		// Create element
 
-		let link = (this.name === 'but' || this.name === 'butdel') ? $(document.createElement('button')) : $(document.createElement('a')); 
+		let link = $(document.createElement(type === 'button'? type : 'a')); 
 		
 		// Turn argument pairs into attribute/value pairs
 	
-		for (let i = 1; i < this.args.length;i+=2) {
-			//'goto' makes a passage link, same processing as default SC
-			if (this.args[i] === 'goto') { 
-				link.attr( 'data-passage' , this.args[i+1] );
-				passage = this.args[i+1];
-				
-				if (Story.has(passage)) {
-					link.addClass('link-internal');
-
-					if (Config.addVisitedLinkClass && State.hasPlayed(passage)) {
-						link.addClass('link-visited');
-					}
-				} else {
-					link.addClass('link-broken');
+		for (let i = 0; i < attributes.length;i+=2) {
+			if (typeof attributes[i] === 'object'){// jquery style object
+				link.attr(attributes[i]);
+				i--;
+			} else { //Simple pair
+				link.attr( attributes[i], attributes[i+1]);
+			}
+		}
+		
+		// Process special args
+		if (link.attr('goto')){
+			var passage = link.attr('goto');
+			link.removeAttr('goto');
+			// Default link processing
+			if (Story.has(passage)) {
+				link.addClass('link-internal');
+				if (Config.addVisitedLinkClass && State.hasPlayed(passage)) {
+					link.addClass('link-visited');
 				}
-			//Check for other special args
-			} else if (this.args[i] === 'append' || this.args[i] === 'app'){
-			
-				toApp.push(this.args[i+1]);
-			
-			} else if (this.args[i] === 'prepend' || this.args[i] === 'prep'){
-			
-				toPrep.push(this.args[i+1]);
-			
-			} else if (this.args[i] === 'replace' || this.args[i] === 'rep'){
-			
-				toRep.push(this.args[i+1]);
-			
 			} else {
-				link.attr( this.args[i] , this.args[i+1] );
+				link.addClass('link-broken');
 			}
 		}
 		
 		//Doesn't take special link syntax so core profile it is
-		link.wikiWithOptions({ profile : 'core' }, this.args[0]);
-		link.addClass(`macro-${this.name}`)
-			.ariaClick({
-				namespace : '.macros',
-				role : (this.name === 'but' || this.name === 'butdel') ? 'button' : 'link',
-				one : (this.name === 'adel' || this.name === 'butdel' || passage != null) ? true : false
-			}, 
-				
-			this.createShadowWrapper(
+		link.wikiWithOptions({ profile : 'core' }, this.args[0])
+			.addClass(`macro-${this.name}`);
 		
-				() => {
-					if (payload !== ''){
-						if (toRep.length + toPrep.length + toApp.length){
-						  //Order: replace, prepend, append
-						  toRep.length ? toRep.forEach((target) => {
-							  $(target).empty().wiki(payload)
-						  }) : '';
-						  toPrep.length ? toPrep.forEach((target) => {
-							  $(document.createDocumentFragment()).wiki(payload).prependTo(target);
-						  }) : '';
-						  toApp.length ? toApp.forEach((target) => {
-							  $(target).wiki(payload)
-						  }) : '';
-						} else {
-						  //Default case
-						  $.wiki(payload);
-						}
-					}
-				},
-			
-				passage != null ?
-				() => Engine.play(passage) : 
-				(this.name === 'adel' || this.name === 'butdel') ?
-				() => link.remove() : null
-			))
-		.appendTo(this.output);
+		link.ariaClick({
+				namespace : '.macros',
+				role : type ,
+				one : (this.name.length > 3 || passage !== undefined) ? true : false},
+			this.createShadowWrapper( function() {
+				$.wiki(payload);
+					
+				Rep ? $(Rep.args[0]).empty().wiki(Rep.contents) : null ;
+				Prep ? $(document.createDocumentFragment())
+								.wiki(Prep.contents).prependTo(Prep.args[0]) : null ;
+				App ? $(App.args[0]).wiki(App.contents) : null ;
+			}, () => {
+				if (passage != null){ //Go to passage
+					Engine.play(passage)
+				} else if (this.name.length > 3){ //Remove link
+					link.remove()
+				}
+			})).appendTo(this.output);
 	}
 });
