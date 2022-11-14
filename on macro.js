c706d0483f2e78d481c5fb1d983bebac4394241d
Maliface <105600295+MalifaciousGames@users.noreply.github.com>
2022-11-14 14:01:50 +0100

    Update on macro.js

--- on macro.js
+++ on macro.js
@@ -6,75 +6,66 @@ Syntax:
 <</on>>
 
 To run the contents again (update it):
-<<trigger 'eventName'>>
-*/
+<<trigger 'eventName'>> */
 	
 Macro.add('on', {
 	tags  : null,
 
 	handler() {
 		
-		if (typeof this.args[0] !== 'string'){
-			return this.error(`Event name must be a string, reading: ${typeof this.args[0]}.`);
-		} else if (this.args[0].length === 0) {
+		if (this.args[0] === undefined || this.args[0] === '') {
 			return this.error(`Missing event name.`);
+		} else if (typeof this.args[0] !== 'string'){
+			return this.error(`Event name must be a string, reading: ${typeof this.args[0]}.`);
 		}
 		
-		// Event names to array + trim white spaces
-
 		let trig = this.args[0].split(',');
-		trig = trig.map(event => event.trim());
-		
 		const content = this.payload[0].contents, attributes = this.args.slice(2);
+		trig = trig.map(event => event.trim());
 			
 		// Create element, apply attributes
-		
 		let container = $(document.createElement(this.args[1] ? this.args[1] : 'span')).addClass(`macro-${this.name}`);
 			
 		for (let i = 0; i < attributes.length;i+=2) {
-			if (typeof attributes[i] === 'object'){
-				//JQuery style object
+			if (typeof attributes[i] === 'object'){ //JQuery style object
 				container.attr(attributes[i]);
 				i--;
 			} else { // Simple pairs
 				container.attr(attributes[i], attributes[i+1]);
 			}
 		}
-			
+
 		// Append to passage 
 		container.wiki(content).appendTo(this.output);
 		
 		// Apply listeners for each event name
-		
 		trig.forEach(event => {
 			if (Config.debug) {
 				console.log(`Listener added for ${event}.`);
 			}
+			customEvents.pushUnique(event);
 			$(document).on(event, function() {
 				container.empty().wiki(content);
-				customEvents.pushUnique(event);
 			});
 		})
 	}
 });
 
-// Triggers custom events
+// Triggers custom event
 
 Macro.add('trigger', {
 	handler() {
 		
-		if (typeof this.args[0] !== 'string'){
-			return this.error(`Event name must be a string, reading: ${typeof this.args[0]}.`);
-		} else if (this.args[0].length === 0) {
+		if (this.args[0] === undefined || this.args[0] === '') {
 			return this.error(`Missing event name.`);
+		} else if (typeof this.args[0] !== 'string'){
+			return this.error(`Event name must be a string, reading: ${typeof this.args[0]}.`);
 		}
 		
-		// Same processing
 		let trig = this.args[0].split(',');
 		trig = trig.map(event => event.trim());
 		
-		// Triggers each supplied event
-		
+		// Triggers each event supplied
 		trig.forEach(event => {
 			$(document).trigger(event);
 			if (Config.debug) {
