5b52c9aaea23a424d0127ea55c77d02574e7b4ba
Maliface <105600295+MalifaciousGames@users.noreply.github.com>
2022-11-14 14:16:40 +0100

    Update a-macro.js

--- a-macro/a-macro.js
+++ a-macro/a-macro.js
@@ -31,15 +31,10 @@ Macro.add(['a','adel','but','butdel'], {
 			return this.error(`No ${type} text specified.`);
 		}
 		const error = [];
-		if (Rep && !Rep.args[0]){
-			error.push(`<<rep>>`);
-		}
-		if (Prep && !Prep.args[0]){
-			error.push(`<<prep>>`);
-		}
-		if (App && !App.args[0]){
-			error.push(`<<app>>`);
-		}
+		Rep && !Rep.args[0] ? error.push(`<<rep>>`) : null;
+		Prep && !Prep.args[0] ? error.push(`<<prep>>`) : null;
+		App && !App.args[0] ? error.push(`<<app>>`) : null;
+		
 		if (error.length){
 			return this.error(`Invalid selector supplied to ${error.join(', ')}.`);
 		}
@@ -49,9 +44,8 @@ Macro.add(['a','adel','but','butdel'], {
 		let link = $(document.createElement(type === 'button'? type : 'a')); 
 		
 		// Turn argument pairs into attribute/value pairs
-	
 		for (let i = 0; i < attributes.length;i+=2) {
-			if (typeof attributes[i] === 'object'){// jquery style object
+			if (typeof attributes[i] === 'object'){// jQuery style object
 				link.attr(attributes[i]);
 				i--;
 			} else { //Simple pair
@@ -59,7 +53,14 @@ Macro.add(['a','adel','but','butdel'], {
 			}
 		}
 		
-		// Process special args
+		//Built-in event trigger
+		if (link.attr('trigger')){
+			var trig = link.attr('trigger').split(',');
+			trig = trig.map(event => event.trim());
+			link.removeAttr('trigger');
+		}
+								 
+		// Goto attribute
 		if (link.attr('goto')){
 			var passage = link.attr('goto');
 			link.removeAttr('goto');
@@ -74,6 +75,20 @@ Macro.add(['a','adel','but','butdel'], {
 			}
 		}
 		
+		// Key attribute
+		if (link.attr('key')){
+			const key = link.attr('key').toUpperFirst(),
+				keyType = (isNaN(Number(key))) ? 'key' : 'keyCode';
+			
+			$(document).on('keyup', function(e) {
+				if (e[keyType] == key){
+					e.preventDefault();
+					link.click();
+					return false;
+				}
+			})
+		}
+		
 		//Doesn't take special link syntax so core profile it is
 		link.wikiWithOptions({ profile : 'core' }, this.args[0])
 			.addClass(`macro-${this.name}`);
@@ -83,18 +98,18 @@ Macro.add(['a','adel','but','butdel'], {
 				role : type ,
 				one : (this.name.length > 3 || passage !== undefined) ? true : false},
 			this.createShadowWrapper( function() {
-				$.wiki(payload);
-					
+				payload ? $.wiki(payload) : null ;
 				Rep ? $(Rep.args[0]).empty().wiki(Rep.contents) : null ;
 				Prep ? $(document.createDocumentFragment())
 								.wiki(Prep.contents).prependTo(Prep.args[0]) : null ;
 				App ? $(App.args[0]).wiki(App.contents) : null ;
+				trig ? trig.forEach(event => {$(document).trigger(event)})  : null ;
 			}, () => {
 				if (passage != null){ //Go to passage
 					Engine.play(passage)
 				} else if (this.name.length > 3){ //Remove link
 					link.remove()
-				}
-		})).appendTo(this.output);
+				}							 
+			})).appendTo(this.output);
 	}
 });
