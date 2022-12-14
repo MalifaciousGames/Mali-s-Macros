39597fd2d52ba47976a45a0bb3057e7b0ab68aba
Maliface <105600295+MalifaciousGames@users.noreply.github.com>
2022-12-14 21:39:07 +0100

    Update a-macro.js

--- a-macro/a-macro.js
+++ a-macro/a-macro.js
@@ -19,37 +19,27 @@ Macro.add(['a','adel','but','butdel'], {
 	tags    : ['rep','prep','app'],
 
 	handler() {
-		
-		const type = (this.name[0] === 'b') ? 'button' : 'link', attributes = this.args.slice(1), payload = this.payload[0].contents.trim();
+	
+		const type = (this.name[0] === 'b') ? 'button' : 'link',
+					attributes = this.args.slice(1),
+					payload = this.payload[0].contents.trim();
 		const Rep = this.payload.find(pay => pay.name === 'rep'),
 					Prep = this.payload.find(pay => pay.name === 'prep'),
 					App = this.payload.find(pay => pay.name === 'app');
 		
-		//Broad errors
-
-		if (this.args.length === 0) {
-			return this.error(`No ${type} text specified.`);
-		}
-		const error = [];
-		Rep && !Rep.args[0] ? error.push(`<<rep>>`) : null;
-		Prep && !Prep.args[0] ? error.push(`<<prep>>`) : null;
-		App && !App.args[0] ? error.push(`<<app>>`) : null;
-		
-		if (error.length){
-			return this.error(`Invalid selector supplied to ${error.join(', ')}.`);
-		}
+		if(this.args[0] === undefined)
+			return this.error(`No ${type} text supplied.`)
 		
 		// Create element
-
 		let link = $(document.createElement(type === 'button'? type : 'a')); 
-		
-		// Turn argument pairs into attribute/value pairs
-		for (let i = 0; i < attributes.length;i+=2) {
-			if (typeof attributes[i] === 'object'){// jQuery style object
+
+		// Turn argument pairs into html property/value pairs
+		for (let i = 0; i < attributes.length;i++) {
+		 if (typeof attributes[i] === 'object'){// jQuery style object
 				link.attr(attributes[i]);
-				i--;
-			} else { //Simple pair
+		 } else { //Simple pairs
 				link.attr( attributes[i], attributes[i+1]);
+				i++;
 			}
 		}
 		
@@ -79,6 +69,7 @@ Macro.add(['a','adel','but','butdel'], {
 		if (link.attr('key')){
 			let keyArray = link.attr('key').split(',');
 			keyArray = keyArray.map(key => key.trim());
+			link.removeAttr('key');
 			
 			// Event handler
 			$(document).on('keyup', function(e) {
@@ -94,27 +85,36 @@ Macro.add(['a','adel','but','butdel'], {
 			})
 		}
 		
+		/* Save old _this */
+		const oldThis = State.temporary.this !== undefined ?
+						State.temporary.this : null; 
+		
 		//Doesn't take special link syntax so core profile it is
 		link.wikiWithOptions({ profile : 'core' }, this.args[0])
 			.addClass(`macro-${this.name}`);
 		
-		link.ariaClick({
-				namespace : '.macros',
-				role : type ,
-				one : (this.name.length > 3 || passage !== undefined) ? true : false},
-			this.createShadowWrapper( function() {
-				payload ? $.wiki(payload) : null ;
-				Rep ? $(Rep.args[0]).empty().wiki(Rep.contents) : null ;
-				Prep ? $(document.createDocumentFragment())
-								.wiki(Prep.contents).prependTo(Prep.args[0]) : null ;
-				App ? $(App.args[0]).wiki(App.contents) : null ;
-				trig ? trig.forEach(event => {$(document).trigger(event)})  : null ;
-			}, () => {
-				if (passage != null){ //Go to passage
-					Engine.play(passage)
-				} else if (this.name.length > 3){ //Remove link
-					link.remove()
-				}							 
+			link.ariaClick({
+					namespace : '.macros',
+					role : type ,
+					one : (this.name.length > 3 || passage !== undefined) ? true : false},
+				this.createShadowWrapper( () => {
+					State.temporary.this = link; /* Add proper shadow value*/
+				
+					payload ? $.wiki(payload) : null ;
+					Rep ? $(Rep.args[0] ?? link.parent())
+									.empty().wiki(Rep.contents) : null ;
+					Prep ? $(document.createDocumentFragment()).wiki(Prep.contents).prependTo(Prep.args[0] ?? link.parent()) : null ;
+					App ? $(App.args[0]  ?? link.parent()).wiki(App.contents) : null ;
+					trig ? trig.forEach(event => {$(document).trigger(event)}) : null ;
+					
+				}, () => {
+					if (passage != null){ //Go to passage
+						Engine.play(passage)
+					} else if (this.name.length > 3){ //Remove link
+						link.remove()
+					}
+				oldThis !== null? State.temporary.this = oldThis : 
+				delete State.temporary.this; /* Shadow cleanup */
 			})).appendTo(this.output);
 	}
 });
