5ddadfcff712977c104b8fcb439d339620acfcc8
Maliface <105600295+MalifaciousGames@users.noreply.github.com>
2023-02-09 19:23:26 +0100

    Update a-macro.js
    Many small improvements, better processing of args

--- a-macro/a-macro.js
+++ a-macro/a-macro.js
@@ -20,42 +20,53 @@ Macro.add(['a','adel','but','butdel'], {
 
 	handler() {
 	
-		const type = (this.name[0] === 'b') ? 'button' : 'link',
-					attributes = this.args.slice(1),
-					payload = this.payload[0].contents.trim();
+		const type = this.name[0] === 'b' ? 'button' : 'link',
+			attributes = this.args.slice(1),
+			payload = this.payload[0].contents;
+      
 		const Rep = this.payload.find(pay => pay.name === 'rep'),
-					Prep = this.payload.find(pay => pay.name === 'prep'),
-					App = this.payload.find(pay => pay.name === 'app');
-		
-		if(this.args[0] === undefined)
-			return this.error(`No ${type} text supplied.`)
-		
+			Prep = this.payload.find(pay => pay.name === 'prep'),
+			App = this.payload.find(pay => pay.name === 'app');
+      	
+      		let oldThis;
+      
 		// Create element
 		let link = $(document.createElement(type === 'button'? type : 'a')); 
-
+		
+		// Catch trigger property
+      		if (attributes.includes('trigger')){
+			var trig = attributes.deleteAt([attributes.indexOf('trigger')+1])[0].split(',');
+			trig = trig.map(e => e.trim());
+			attributes.delete('trigger');
+		};
+      
+      		// Catch goto property
+		if (attributes.includes('goto')){
+			var passage = attributes.deleteAt([attributes.indexOf('goto')+1])[0];
+			attributes.delete('goto');
+		};
+      	
+      		// Catch key property
+		if (attributes.includes('key')){
+			var keyArray = attributes.deleteAt([attributes.indexOf('key')+1])[0].split(',');
+          	console.log(keyArray)
+			attributes.delete('key');
+		};
+      
 		// Turn argument pairs into html property/value pairs
 		for (let i = 0; i < attributes.length;i++) {
-		 if (typeof attributes[i] === 'object'){// jQuery style object
+			if (typeof attributes[i] === 'object'){// jQuery style object
 				link.attr(attributes[i]);
-		 } else { //Simple pairs
+			} else { //Simple pairs
 				link.attr( attributes[i], attributes[i+1]);
 				i++;
 			}
-		}
-		
-		//Built-in event trigger
-		if (link.attr('trigger')){
-			var trig = link.attr('trigger').split(',');
-			trig = trig.map(event => event.trim());
-			link.removeAttr('trigger');
-		}
-								 
-		// Goto attribute
-		if (link.attr('goto')){
-			var passage = link.attr('goto');
-			link.removeAttr('goto').attr('data-passage', passage);
-			// Default link processing
-			if (Story.has(passage)) {
+		};
+      	
+      		// Process passage links like SC's link macro would
+      		if (passage) {
+        		link.attr('data-passage', passage);
+        		if (Story.has(passage)) {
 				link.addClass('link-internal');
 				if (Config.addVisitedLinkClass && State.hasPlayed(passage)) {
 					link.addClass('link-visited');
@@ -63,16 +74,13 @@ Macro.add(['a','adel','but','butdel'], {
 			} else {
 				link.addClass('link-broken');
 			}
-		}
-		
-		// Key attribute
-		if (link.attr('key')){
-			let keyArray = link.attr('key').split(',');
-			keyArray = keyArray.map(key => key.trim());
-			link.removeAttr('key');
-			
+		};
+
+     
+		// Attach key listeners
+		if (keyArray){
 			// Event handler
-			$(document).on('keyup', function(e) {
+			$(document).keyup((e) => {
 				keyArray.every(key => {
 					if (e[isNaN(Number(key)) ? 'key' : 'keyCode'] == key){
 						e.preventDefault();
@@ -83,38 +91,37 @@ Macro.add(['a','adel','but','butdel'], {
 					return true;
 				})
 			})
-		}
+		};
 		
-		/* Save old _this */
-		const oldThis = State.temporary.this !== undefined ?
-						State.temporary.this : null; 
-		
-		//Doesn't take special link syntax so core profile it is
-		link.wikiWithOptions({ profile : 'core' }, this.args[0])
-			.addClass(`macro-${this.name}`);
-		
-			link.ariaClick({
-					namespace : '.macros',
-					role : type ,
-					one : (this.name.length > 3 || passage !== undefined) ? true : false},
-				this.createShadowWrapper( () => {
-					State.temporary.this = link; /* Add proper shadow value*/
-				
-					payload ? $.wiki(payload) : null ;
-					Rep ? $(Rep.args[0] ?? link.parent())
-									.empty().wiki(Rep.contents) : null ;
+      		// Wiki link text
+		link.wikiWithOptions({ profile : 'core' }, this.args[0]).addClass(`macro-${this.name}`);
+      
+		link.ariaClick( //Options object
+			{namespace : '.macros',
+			role : type ,
+			one : (this.name.length > 3 || passage !== undefined) ? true : false},            
+			this.createShadowWrapper(
+				() => { // Main call
+          			console.log('Click ran');
+        			oldThis = State.temporary.hasOwnProperty('this') ? State.temporary.this : null; 
+           			State.temporary.this = link;
+        	
+        			payload ? $.wiki(payload) : null;
+					Rep ? $(Rep.args[0] ?? link.parent()).empty().wiki(Rep.contents) : null ;
 					Prep ? $(document.createDocumentFragment()).wiki(Prep.contents).prependTo(Prep.args[0] ?? link.parent()) : null ;
 					App ? $(App.args[0]  ?? link.parent()).wiki(App.contents) : null ;
-					trig ? trig.forEach(event => {$(document).trigger(event)}) : null ;
-					
-				}, () => {
-					if (passage != null){ //Go to passage
-						Engine.play(passage)
+					trig ? trig.forEach(e => {$(document).trigger(e)}) : null ;
+				},
+				() => { // After call
+        			console.log('After ran');
+	    			oldThis !== null ? State.temporary.this = oldThis : delete State.temporary.this; // _this cleanup
+           			if (passage){ //Go to passage
+						Engine.play(passage);
 					} else if (this.name.length > 3){ //Remove link
-						link.remove()
+						link.remove();
 					}
-				oldThis !== null? State.temporary.this = oldThis : 
-				delete State.temporary.this; /* Shadow cleanup */
-			})).appendTo(this.output);
+				}
+			)
+		).appendTo(this.output);
 	}
 });
