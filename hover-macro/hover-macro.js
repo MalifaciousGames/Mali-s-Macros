7414d6742389c84c1024b0eff5cd8e302c3ba482
Maliface <105600295+MalifaciousGames@users.noreply.github.com>
2023-03-06 19:51:56 +0100

    Updated with bundle

--- hover-macro/hover-macro.js
+++ hover-macro/hover-macro.js
@@ -1,79 +1,58 @@
+/* ----------------------- UTILITY BUNDLE ----------------------- */
+/* ------------------- ONLY ONE COPY NEEDED! -------------------- */
+
+window.MalisMacros={wikiWrapper:function(t,e){const r={};"object"==typeof t&&$.each(t,((t,e)=>{r[t]=State.temporary?.[t],State.temporary[t]=e}));try{e()}finally{$.each(r,((t,e)=>{State.temporary[t]=e}))}},on_macro_events:[],version: '1.0'},Array.prototype.attrFinder=function(t,e){const r=this.indexOf(t);if(-1!==r){const[a]=this.deleteAt(r+1);return this.delete(t),e&&e.attr(`data-${t}`,a),a}return!1},Array.prototype.payloadsToObj=function(){const t={default:this[0].contents};return this.slice(1).forEach((e=>{t[e.name]=e})),t},Array.prototype.unpack=function(){let t=0,e=this;for(;t<e.length;){const r=e[t];Array.isArray(r)?e.deleteAt(t)[0].forEach((t=>e.push(t))):"object"!=typeof r||r.isLink?t++:$.each(e.deleteAt(t)[0],((t,r)=>{e.push(t.toLowerCase()),e.push(r)}))}if(e.length%2)throw new Error("Non-object arguments should always come in pairs. "+(e.includes("disabled")?"Even the 'disabled' attribute.":""));return e},$.fn.extend({applyAttr:function(t){for(let e=0;e<t.length;e+=2)this.attr(t[e],t[e+1]);return this},runOutput:function(t,e){if(e)switch(t){case"rep":$(e.args[0]??this.parent()).empty().wiki(e.contents);break;case"prep":$(document.createDocumentFragment()).wiki(e.contents).prependTo(e.args[0]??this.parent());break;case"app":$(e.args[0]??this.parent()).wiki(e.contents);break;case"diag":Dialog.setup(e.args?.[0],e.args?.[1]),Dialog.wiki(e.contents),Dialog.open();break;case"refresh":this.empty().wiki(e);break;default:$.wiki(e)}},diagFrom:function(t,e){const r=this.offset().top-this.height()/2,a=this.offset().left-this.width()/2;return{distance:Math.hypot(r-e,a-t),top:e-r,left:t-a}}});
+
+/* ------------------- END OF UTILITY BUNDLE -------------------- */
+
+/* Mali's <<hover>> macro for Sgarcube*/
+
 Macro.add('hover', {
 	tags : ['swap','tip'],
 	
 	handler() {
-		const swap = this.payload.find(pay => pay.name === 'swap'),
-			tip = this.payload.find(pay => pay.name === 'tip'),
-			contAttr = this.args.slice(1);
-		
-		if (swap && !swap.contents) {
-			//Empty <<swap>> has very problematic behavior, thus the error
-			return this.error('<<swap>> tag has no content.');
-		}
+
+      	const payloads = this.payload.payloadsToObj(this.self.tags),
+              hasTip = payloads?.tip ? true : false,
+              hasSwap = payloads?.swap ? true : false,
+              contAttr = this.args.slice(1).unpack();
+
+		if (hasSwap && payloads.swap.contents.trim() === '') return this.error('<<swap>> tag needs to have content.');
 		
 		//Create outer + inner container
 		const container = $(document.createElement(this.args[0]||'span')),
-			innerCont = $('<span>').wiki(this.payload[0].contents);
-		
-		container.append(innerCont);
+			innerCont = $('<span>').wiki(this.payload[0].contents).addClass('macro-hover-inner');
 		
 		// Catch capture mode!
-		if (contAttr.includes('capture')){
-			var capture = contAttr.deleteAt([contAttr.indexOf('capture')+1])[0];
-			contAttr.delete('capture');
-		}
-		
-		function applyAttr(elem,array) {
-			for (let i = 0; i < array.length;i++) {
-				if (typeof array[i] === 'object'){//JQuery style object
-					elem.attr(array[i]);
-				} else { // Simple pairs
-					elem.attr(array[i], array[i+1]);
-					i++;
-				}
-			}
-			return elem;
-		}
+		const capture = contAttr.attrFinder('capture');
 
-		applyAttr(container, contAttr);
+		container.applyAttr(contAttr).append(innerCont);
 		
-		if (tip) {//Create tip elem, add attributes
-			var tipElem = $('<span>');
-			applyAttr(tipElem, tip.args).addClass('macro-hover-tip');
-			if (capture) {tipElem.wiki(tip.contents)};
+		if (hasTip) {//Create tip elem, add attributes
+			var tipElem = $('<span>').applyAttr(payloads.tip.args.unpack()).addClass('macro-hover-tip');
+			if (capture) {tipElem.wiki(payloads.tip.contents)};
 			container.append(tipElem);
 		}
 		
-		if (swap) {//Add attributes to inner container (mostly for targetting)
-			applyAttr(innerCont, swap.args);
-			if (capture) {
-				var shadowSwap = $(document.createDocumentFragment()).wiki(swap.contents),
-				    shadowCont = $(document.createDocumentFragment()).wiki(this.payload[0].contents);	
-			};
+		if (hasSwap && capture) {
+			var shadowSwap = $(document.createDocumentFragment()).wiki(payloads.swap.contents),
+				shadowCont = $(document.createDocumentFragment()).wiki(this.payload[0].contents);	
 		}
-		
-		innerCont.addClass('macro-hover-inner');
 
 		$(container).hover(this.createShadowWrapper(() => {//In
-			if (swap) {
+			if (hasSwap) {
 				innerCont.empty();
-				capture ?
-					innerCont.append(clone(shadowSwap)) :
-					innerCont.wiki(swap.contents);
+				capture ? innerCont.append(clone(shadowSwap)) : innerCont.wiki(payloads.swap.contents);
 			}
-			if (tip) {
-				tipElem.empty().wiki(tip.contents);
+			if (hasTip && !capture) {
+				tipElem.empty().wiki(payloads.tip.contents);
 			}
 		}),
 		this.createShadowWrapper(() => {//Out
-			if (swap) {
+			if (hasSwap) {
 				innerCont.empty();
-				capture ?
-					innerCont.append(clone(shadowCont)) :
-					innerCont.wiki(this.payload[0].contents);
+				capture ? innerCont.append(clone(shadowCont)) : innerCont.wiki(this.payload[0].contents);
 			}
-		}));
-		
-	container.addClass('macro-hover').appendTo(this.output);
+		})).addClass('macro-hover').appendTo(this.output);
 	}
 });
