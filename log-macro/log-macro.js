f8b045c7f6c86f5e309ce095598b05460ae45d22
Maliface <105600295+MalifaciousGames@users.noreply.github.com>
2023-03-06 14:23:31 +0100

    Added <<logsize>>

--- log-macro/log-macro.js
+++ log-macro/log-macro.js
@@ -5,9 +5,23 @@ const timers = {},
 	getTime = (id) => {
     	const time = String(Date.now()-timers[id].time);
     	return time.length > 3 ? time.splice(-3,0,'.')+' s' : time+' ms';
-    };
+    },
+	getByteSize = (data) => {
+		let totBytes = 0;
+        	switch (typeof data) {
+              case 'boolean' : totBytes += 4; break;
+              case 'number' : totBytes += 8; break;
+              case 'string' : totBytes += 2*data.length; break;
+              case 'function' : totBytes += 2*JSON.stringify(data).length; break;
+              case 'object' : //Arrays aren't special in that regard
+                for (const key in data) {totBytes += getByteSize(key) + getByteSize(data[key])};
+                if (data instanceof Date) {totBytes += 8};
+                break;
+            } 
+        	return totBytes;
+	};
 
-Macro.add(['log','logtime','logstop'], {
+Macro.add(['log','logtime','logstop','logsize'], {
 	handler() {
       
       if (['logtime','logstop'].includes(this.name)) {
@@ -15,6 +29,7 @@ Macro.add(['log','logtime','logstop'], {
         
         if (this.name === 'logstop') {//Delete timer
           	if (timers[id] === undefined) return this.error(`'${id}' timer does not exist.`);
+          
         	console.log(`%c${id === 'log-time' ? 'Default' : "'"+id+"'"} timer ended after ${getTime(id)}`, timers[id].style);
           	delete timers[id];
         } else if (timers[id]) {//ID exists, log it
@@ -24,11 +39,13 @@ Macro.add(['log','logtime','logstop'], {
           	console.log(`%cStarted timer ${id === 'log-time' ? '' : ": '"+id+"'"}`, style);
           	timers[id] = {time : Date.now(), style : style};
         }
-        
+      } else if (this.name === 'logsize') {
+        const size = getByteSize(this.args[0]).toLocaleString();
+        console.log(`%c${this.args[1] ? this.args[1] + '\'s size' : 'Size'} is ${size} bytes.`, this.args[2]);
       } else  if (!this.args.length) {//Source syntax
         
       	if (this.parent) {//Not from passage load
-		let source = this.parent.source, index = source.indexOf('<<log>>');
+			let source = this.parent.source, index = source.indexOf('<<log>>');
           	source = source.includes('<<log>>') ? source.splice(index ,0,'%c').splice(index+9 ,0,'%c') : `%c<<log>> %cran from %c${source}.`;
         	console.log(source, 'font-weight:bold;color:red','');
         } else {
