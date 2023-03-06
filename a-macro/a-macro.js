621f464687e7957e14cbe7414eee3f121f8a5c18
Maliface <105600295+MalifaciousGames@users.noreply.github.com>
2023-03-06 17:12:19 +0100

    Modular, with bundle

--- a-macro/a-macro.js
+++ a-macro/a-macro.js
@@ -1,86 +1,58 @@
-// Maliface's 'a' macro for Sugarcube
-;(function(){
-  
-const attrFinder = (attributes, match) => {//Return proper arg from attributes and remove pair
-	const attr = attributes.deleteAt([attributes.indexOf(match)+1])[0];
-	attributes.delete(match);
-    return attr;
-};
+/* ----------------------- UTILITY BUNDLE ----------------------- */
+/* ------------------- ONLY ONE COPY NEEDED! -------------------- */
+
+window.MalisMacros={wikiWrapper:function(t,e){const r={};"object"==typeof t&&$.each(t,((t,e)=>{r[t]=State.temporary?.[t],State.temporary[t]=e}));try{e()}finally{$.each(r,((t,e)=>{State.temporary[t]=e}))}},on_macro_events:[]},Array.prototype.attrFinder=function(t,e){const r=this.indexOf(t);if(-1!==r){const[a]=this.deleteAt(r+1);return this.delete(t),e&&e.attr(`data-${t}`,a),a}return!1},Array.prototype.payloadsToObj=function(){const t={default:this[0].contents};return this.slice(1).forEach((e=>{t[e.name]=e})),t},Array.prototype.unpack=function(){let t=0,e=this;for(;t<e.length;){const r=e[t];Array.isArray(r)?e.deleteAt(t)[0].forEach((t=>e.push(t))):"object"!=typeof r||r.isLink?t++:$.each(e.deleteAt(t)[0],((t,r)=>{e.push(t.toLowerCase()),e.push(r)}))}if(e.length%2)throw new Error("Non-object arguments should always come in pairs. "+(e.includes("disabled")?"Even the 'disabled' attribute.":""));return e},$.fn.extend({applyAttr:function(t){for(let e=0;e<t.length;e+=2)this.attr(t[e],t[e+1]);return this},runOutput:function(t,e){if(e)switch(t){case"rep":$(e.args[0]??this.parent()).empty().wiki(e.contents);break;case"prep":$(document.createDocumentFragment()).wiki(e.contents).prependTo(e.args[0]??this.parent());break;case"app":$(e.args[0]??this.parent()).wiki(e.contents);break;case"diag":Dialog.setup(e.args?.[0],e.args?.[1]),Dialog.wiki(e.contents),Dialog.open();break;case"refresh":this.empty().wiki(e);break;default:$.wiki(e)}},diagFrom:function(t,e){const r=this.offset().top-this.height()/2,a=this.offset().left-this.width()/2;return{distance:Math.hypot(r-e,a-t),top:e-r,left:t-a}}});
+
+/* ------------------- END OF UTILITY BUNDLE -------------------- */
+
+/* Mali's <<a>> macro for Sugarcube */
 
 Macro.add(['a','adel','but','butdel'], {
 	isAsync : true,
-	tags    : ['rep','prep','app'],
+	tags    : ['rep','prep','app','diag'],
 
 	handler() {
       
 		const type = this.name[0] === 'b' ? 'button' : 'link',
-		      attributes = this.args.slice(1),
-		      payload = this.payload[0].contents,
-		      Rep = this.payload.find(pay => pay.name === 'rep'),
-		      Prep = this.payload.find(pay => pay.name === 'prep'),
-		      App = this.payload.find(pay => pay.name === 'app');
-      	
-      		let oldThis, passage, i = 0, setter, 
-            		linkContent = this.args[0],
-            		link = $(`<${type === 'button'? type : 'a'}>`);
+		      attributes = this.args.slice(1).unpack(),
+		      payloads = this.payload.payloadsToObj(this.self.tags);
+
+      	let oldThis, passage, setter, linkContent = this.args[0], link = $(`<${type === 'button'? type : 'a'}>`);
 		
 		//Process bracket syntax
-      		if (typeof linkContent === 'object'){
-          		if (linkContent.setFn){//Has a setter
-        			setter = linkContent.setFn;
-        		}
-        		if (linkContent.isImage) {//[img[url][passage]]
-            			passage = linkContent?.link;
+      	if (typeof linkContent === 'object'){
+        	if (linkContent.setFn){//Has a setter
+        		setter = linkContent.setFn;
+        	}
+        	if (linkContent.isImage) {//[img[url][passage]]
+            	passage = linkContent?.link;
 				linkContent = `<img src=${linkContent.source} class=link-image>`;
 			} else {//[[Text|passage]]
-              			passage = linkContent.link;
+              	passage = linkContent.link;
 				linkContent = linkContent.text;
 			}
-        	}
+        }
       
-      		//Flatten objects into attributes pairs, while handles nested objects
-      		while (i < attributes.length) {
-        		const attr = attributes[i];
-          		if (Array.isArray(attr)) {//Array of pairs
-             			attributes.deleteAt(i)[0].forEach((el) => attributes.push(el));
-            		} else if (typeof attr === 'object' && !attr.isLink){//JQ-style object, provision for goto [[passage]]
-            			$.each(attributes.deleteAt(i)[0] , (key, value)=> {
-                			attributes.push(key.toLowerCase());
-                  			attributes.push(value);
-                		});
-            		} else {
-          			i++;
-            		}
-        	}
-      
-      		//Check all attributes are pairs
-      		if (attributes.length%2) return this.error(`Non-object arguments should always come in pairs. ${attributes.includes('disabled') ? "Even the 'disabled' attribute." : ''}`);
-      
-      		// Catch choice property
-      		if (attributes.includes('choice')){
-			var choiceID = attrFinder(attributes, 'choice');
-        		link.attr('data-choice', choiceID);
-          		choiceID = choiceID.split(',');
+      	// Catch choice property
+      	if (attributes.includes('choice')){
+			var choiceID = attributes.attrFinder('choice', link);
+          	choiceID = choiceID.split(',');
 		};
       
-      		// Catch trigger property
-      		if (attributes.includes('trigger')){
-			var trig = attrFinder(attributes, 'trigger').split(',').map(e => e.trim());
+      	// Catch trigger property
+      	if (attributes.includes('trigger')){
+			var trig = attributes.attrFinder('trigger').split(',').map(e => e.trim());
 		};
       
-      		// Catch goto property
-		if (attributes.includes('goto')){
-			passage = attrFinder(attributes, 'goto');
-          		if (typeof passage === 'object') {passage = passage.link}; 
-		};
+      	// Catch goto property
+		passage = attributes.attrFinder('goto');
       	
-      		// Catch key property
+      	// Catch key property
 		if (attributes.includes('key')){
-			var keyArray = attrFinder(attributes, 'key');
-          		link.attr('data-key', keyArray);
-          		keyArray = keyArray.split(',');
+			var keyArray = attributes.attrFinder('key', link);
+          	keyArray = keyArray.split(',');
 
-          		$(document).keyup('macro-a-key', (e) => {
+          	$(document).keyup('macro-a-key', (e) => {
 				keyArray.every(key => {
 					if (e[isNaN(Number(key)) ? 'key' : 'keyCode'] == key){
 						e.preventDefault();
@@ -93,15 +65,15 @@ Macro.add(['a','adel','but','butdel'], {
 			})
 		};
           
-		// Apply arguments
-		for (let i = 0; i < attributes.length;i+=2) {
-			link.attr(attributes[i], attributes[i+1]);
-		};
-      	
-      		// Process passage links like SC's link macro would
-      		if (passage) {
-          		link.attr('data-passage', passage);
-        		if (Story.has(passage)) {
+      	link.applyAttr(attributes);
+      
+      	// Process passage links like SC's link macro would
+      	if (passage) {
+          	if (typeof passage === 'object') {
+            	passage = passage.link;
+            }
+          	link.attr('data-passage', passage);
+        	if (Story.has(passage)) {
 				link.addClass('link-internal');
 				if (Config.addVisitedLinkClass && State.hasPlayed(passage)) {
 					link.addClass('link-visited');
@@ -109,40 +81,39 @@ Macro.add(['a','adel','but','butdel'], {
 			} else {
 				link.addClass('link-broken');
 			}
-        	};
+        };
 
 		// Wiki link text
 		link.wikiWithOptions({ profile : 'core' }, linkContent).addClass(`macro-${this.name} link-internal`);
-      
-		link.ariaClick( //Options object
+
+      	link.ariaClick( //Options object
 			{namespace : '.macros',
 			role : type ,
-			one : (this.name.length > 3 || passage !== undefined) ? true : false},            
-			this.createShadowWrapper(
-				() => { // Main call
-        				oldThis = State.temporary.hasOwnProperty('this') ? State.temporary.this : null; 
-           				State.temporary.this = link;
-        	
-        				if (payload) {$.wiki(payload)};
-                  			if (setter) {setter()};
-					if (Rep) {$(Rep.args[0] ?? link.parent()).empty().wiki(Rep.contents)};
-					if (Prep) {$(document.createDocumentFragment()).wiki(Prep.contents).prependTo(Prep.args[0] ?? link.parent())};
-					if (App) {$(App.args[0]  ?? link.parent()).wiki(App.contents)};
-					if (trig) {trig.forEach(e => {$(document).trigger(e)})};
-				},
-				() => { // After call
-	    				oldThis !== null ? State.temporary.this = oldThis : delete State.temporary.this; // _this cleanup
-           				if (passage){ //Go to passage
-						Engine.play(passage);
-					} else if (this.name.length > 3){ //Remove link
-						link.remove();
-					}
-                  			if (choiceID){
-                    				choiceID.forEach(id => { $(`[data-choice*=${id}]`).not(link).remove() });
-                    			}
+			one : (this.name.length > 3 || passage) ? true : false},
+        this.createShadowWrapper(
+          MalisMacros.wikiWrapper.bind(this, {'link' : link},
+			function() { // Main call with wikiWrapper
+        		if (setter) {setter()};
+				$.each(payloads, (key, pay) => {
+            		link.runOutput(key, pay);
+            	});
+				if (trig) {
+                	trig.forEach(e => {
+                    	$(document).trigger(e)
+                    })
+                };
+			}),
+          
+        	function() { // After call
+				if (passage){ //Go to passage
+					Engine.play(passage);
+				} else if (this.name.length > 3){ //Remove link
+					link.remove();
 				}
-			)
-		).appendTo(this.output);
+            	if (choiceID){
+            		choiceID.forEach(id => { $(`[data-choice*=${id}]`).not(link).remove() });
+            	}
+			})
+      ).appendTo(this.output);
 	}
 });
-})();
