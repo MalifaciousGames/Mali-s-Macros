eddacb783872b917803eab391672db5d167bab8b
Maliface <105600295+MalifaciousGames@users.noreply.github.com>
2023-03-07 11:09:18 +0100

    Updated with bundle + when syntax

--- listen-macro/listen.js
+++ listen-macro/listen.js
@@ -1,40 +1,34 @@
+/* ----------------------- UTILITY BUNDLE ----------------------- */
+/* ------------------- ONLY ONE COPY NEEDED! -------------------- */
+
+window.MalisMacros={wikiWrapper:function(t,e){const r={};"object"==typeof t&&$.each(t,((t,e)=>{r[t]=State.temporary?.[t],State.temporary[t]=e}));try{e()}finally{$.each(r,((t,e)=>{State.temporary[t]=e}))}},on_macro_events:[],version: '1.0'},Array.prototype.attrFinder=function(t,e){const r=this.indexOf(t);if(-1!==r){const[a]=this.deleteAt(r+1);return this.delete(t),e&&e.attr(`data-${t}`,a),a}return!1},Array.prototype.payloadsToObj=function(){const t={default:this[0].contents};return this.slice(1).forEach((e=>{t[e.name]=e})),t},Array.prototype.unpack=function(){let t=0,e=this;for(;t<e.length;){const r=e[t];Array.isArray(r)?e.deleteAt(t)[0].forEach((t=>e.push(t))):"object"!=typeof r||r.isLink?t++:$.each(e.deleteAt(t)[0],((t,r)=>{e.push(t.toLowerCase()),e.push(r)}))}if(e.length%2)throw new Error("Non-object arguments should always come in pairs. "+(e.includes("disabled")?"Even the 'disabled' attribute.":""));return e},$.fn.extend({applyAttr:function(t){for(let e=0;e<t.length;e+=2)this.attr(t[e],t[e+1]);return this},runOutput:function(t,e){if(e)switch(t){case"rep":$(e.args[0]??this.parent()).empty().wiki(e.contents);break;case"prep":$(document.createDocumentFragment()).wiki(e.contents).prependTo(e.args[0]??this.parent());break;case"app":$(e.args[0]??this.parent()).wiki(e.contents);break;case"diag":Dialog.setup(e.args?.[0],e.args?.[1]),Dialog.wiki(e.contents),Dialog.open();break;case"refresh":this.empty().wiki(e);break;default:$.wiki(e)}},diagFrom:function(t,e){const r=this.offset().top-this.height()/2,a=this.offset().left-this.width()/2;return{distance:Math.hypot(r-e,a-t),top:e-r,left:t-a}}});
+
+/* ------------------- END OF UTILITY BUNDLE -------------------- */
+
+/* Mali's <<listen>> macro for Sgarcube*/
+
 Macro.add('listen', {
-	tags: ['then'],
+	tags: ['when'],
 	isAsync : true,
 	
 	handler() {
-	
-		const payload = this.payload.find(pay => pay.name === 'then'),
-			event = (this.args[0] ? this.args[0].replaceAll(',',' ') : 'change'),
-			attributes = this.args.slice(2);
-		
-		function runCode(e) {
-			try {
-				if (State.temporary.hasOwnProperty('event')) {
-					var eventVar = State.temporary.event;
-				}
-				State.temporary.event = e;
-				$.wiki(payload?.contents);
-			} finally {
-				eventVar !== undefined ?
-					State.temporary.target = eventVar :
-					delete State.temporary.target;
-			}
-		}
 
-		const wrapper = $(document.createElement(this.args[1] || 'span'));
-				
-		for (let i = 0; i < attributes.length;i++) {
-			if (typeof attributes[i] === 'object'){
-				//JQuery style object
-				wrapper.attr(attributes[i]);
-			} else { // Simple pairs
-				wrapper.attr(attributes[i], attributes[i+1]);
-				i++;
-			}
-		}
-		
-		wrapper.on(event, this.createShadowWrapper(runCode));
-		wrapper.wiki(this.payload[0].contents).appendTo(this.output);
+	const payloads = {}, events = [], wrapper = $(document.createElement(this.args[0] || 'span'));
+      
+        wrapper.applyAttr(this.args.slice(1).unpack());
+      
+      	this.payload.slice(1).forEach(tag => {
+          	const event = tag.args[0]?.toLowerCase() ?? 'change';
+        	payloads[event] = tag.contents;
+          	events.push(event);
+        });
+
+	wrapper.on(events.join(' '), this.createShadowWrapper(
+        	(e) => {
+        		MalisMacros.wikiWrapper({event : e},
+                	() => {$.wiki(payloads[e.type])}
+                )
+        	}
+        )).wiki(this.payload[0].contents).appendTo(this.output);
 	}
 });
