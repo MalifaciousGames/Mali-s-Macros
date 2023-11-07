bdde71a5d5b33e7b077d768b03b7afdd98b98497
Maliface <MalifaciousGames@proton.me>
2023-11-07 20:25:35 +0100

    Update listen.js

--- listen-macro/listen.js
+++ listen-macro/listen.js
@@ -1,46 +1,65 @@
 /* Mali's <<listen>> macro for Sgarcube */
 
 Macro.add('listen', {
-	tags: ['when'],
-	isAsync : true,
-    argsToObj : function(args) {
-        let argObject = {}, i = 0;
-        while(i < args.length){
+    tags: ['when'],
+    isAsync: true,
+    argsToObj: function(args) {
+        let argObject = {},
+            i = 0;
+        while (i < args.length) {
             const arg = args[i];
-            if (Array.isArray(arg)) {//An array, splice into position
+            if (Array.isArray(arg)) { //An array, splice into position
                 args.splice(i--, 1, ...arg);
-            } else if (typeof arg === 'object') {//Merge objects!
+            } else if (typeof arg === 'object') { //Merge objects!
                 Object.assign(argObject, arg);
-            } else {//Following pairs
-                const val = args[i+=1];
-                if (val === undefined){throw new Error('Uneven number of arguments.')};
+            } else { //Following pairs
+                const val = args[i += 1];
+                if (val === undefined) {
+                    throw new Error('Uneven number of arguments.')
+                };
                 argObject[arg.toLowerCase()] = val;
             }
             i++;
         }
         return argObject;
     },
-	handler() {
-	    const payloads = {}, events = [], wrapper = $(`<${this.args[0] || 'span'}>`).attr(this.self.argsToObj(this.args.slice(1))).addClass(`macro-${this.name}`);
-        let oldEvent;
-      	this.payload.slice(1).forEach(tag => {
-          	const event = tag.args[0]?.toLowerCase() ?? 'change';
-          	event.split(/\s|,/g).forEach(e => {
-              	if (e) { //Not empty string
-        			payloads[e] = tag.contents;
-          			events.push(e);
-                }
+    handler() {
+        const payloads = {},
+            events = [],
+            attr = this.self.argsToObj(this.args),
+            st = State.temporary,
+            $wr = $(`<${attr.type || 'span'}>`).wiki(this.payload[0].contents);
+        delete attr.type;
+
+        let oldEvent, $trg = $wr;
+
+        if (attr.hasOwnProperty('filter')) {
+            $trg = $wr.find(attr.filter);
+            delete attr.filter;
+        }
+
+        this.payload.slice(1).forEach(tag => {
+            let ev = ['change'];
+            if (tag.args.length) {
+                ev = tag.args.flat(Infinity).map(t => t.split(/\s|,/g)).flat().filter(t => t);
+            }
+            ev.forEach(e => {
+                payloads[e] = tag.contents;
+                events.push(e);
             });
+
         });
 
-	wrapper.on(events.join(' '), this.createShadowWrapper((e) => {
-        try {
-            oldEvent = clone(State.temporary.event);
-            State.temporary.event = e;
+        $trg.on(events.join(' '), this.createShadowWrapper(e => {
+            try {
+                oldEvent = st.event;
+                st.event = e.originalEvent ? ? e;
 
-            $.wiki(payloads[e.type])
-        } finally {
-            oldEvent !== undefined ? State.temporary.event = oldEvent : delete State.temporary.event;
-        }
-    })).wiki(this.payload[0].contents).appendTo(this.output);
-}});
+                $.wiki(payloads[e.type]);
+            } finally {
+                oldEvent !== undefined ? st.event = oldEvent : delete st.event;
+            }
+        }));
+        $wr.attr(attr).addClass(`macro-${this.name}`).appendTo(this.output);
+    }
+});
